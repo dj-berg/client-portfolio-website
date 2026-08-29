c2d059ae7b378b5fe50ed71ff49eee396cf41947
Daniel Berg <danielberg313@gmail.com>
2026-08-29 12:46:50 -0700

    Update emailJS variables

--- script.js
+++ script.js
@@ -340,9 +340,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const submitButton = contactForm?.querySelector('button[type="submit"]');
 
     if (contactForm && statusMessage && submitButton) {
-        const EMAILJS_SERVICE_ID = "service_3uvsbni";
-        const EMAILJS_TEMPLATE_ID = "template_h9nlekg";
-        const EMAILJS_PUBLIC_KEY = "VkVXmCBWffsrQg6wy";
+        const EMAILJS_SERVICE_ID = "service_2164zqr";
+        const EMAILJS_TEMPLATE_ID = "template_dz2dgwf";
+        const EMAILJS_PUBLIC_KEY = "Wbb1rAfuCioW8KCtJ";
         let statusHideTimer = null;
 
         const clearStatusMessage = () => {
